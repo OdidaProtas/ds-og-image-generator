aaf3e7114e5d1a278bd687f626ff935257fe912c
OdidaProtas <bryodiiidah@gmail.com>
2022-10-28 21:42:15 +0300

    templates

--- pages/api/og.jsx
+++ pages/api/og.jsx
@@ -9,22 +9,64 @@ export default function () {
     (
       <div
         style={{
-          fontSize: 128,
-          background: "white",
-          width: "100%",
-          height: "100%",
           display: "flex",
-          textAlign: "center",
+          height: "100%",
+          width: "100%",
           alignItems: "center",
           justifyContent: "center",
+          letterSpacing: "-.02em",
+          fontWeight: 700,
+          background: "white",
         }}
       >
-        Hello world!
+        <div
+          style={{
+            left: 42,
+            top: 42,
+            position: "absolute",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <span
+            style={{
+              width: 24,
+              height: 24,
+              background: "black",
+            }}
+          />
+          <span
+            style={{
+              marginLeft: 8,
+              fontSize: 20,
+            }}
+          >
+            Dreamercodes
+          </span>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            padding: "20px 50px",
+            margin: "0 42px",
+            fontSize: 40,
+            width: "auto",
+            maxWidth: 550,
+            textAlign: "center",
+            backgroundColor: "#4169e1",
+            color: "yellow",
+            lineHeight: 1.4,
+          }}
+        >
+          Learn software development, for free.
+        </div>
       </div>
     ),
     {
-      width: 400,
-      height: 200,
+      width: 1080,
+      height: 720,
     }
   );
 }
